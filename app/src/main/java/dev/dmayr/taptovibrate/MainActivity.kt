84825619e90fe7f128f1347217e19570252de7af
Dennis Mayr <dmayr.dev@gmail.com>
2021-06-23 13:40:09 -0400

    Added extra button, refactored to Long and Short vibrations

--- app/src/main/java/dev/dmayr/taptovibrate/MainActivity.kt
+++ app/src/main/java/dev/dmayr/taptovibrate/MainActivity.kt
@@ -16,16 +16,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.button.setOnClickListener {
-            vibratePhone()
+        binding.buttonShort.setOnClickListener {
+            vibratePhoneShort()
+        }
+        binding.buttonLong.setOnClickListener {
+            vibratePhoneLong()
         }
     }
 
     @Suppress("DEPRECATION")
-    private fun vibratePhone() {
+    private fun vibratePhoneShort() {
         val vibration = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             vibration.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE))
         } else vibration.vibrate(150)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun vibratePhoneLong() {
+        val vibration = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibration.vibrate(VibrationEffect.createOneShot(250, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else vibration.vibrate(250)
     }
 }
