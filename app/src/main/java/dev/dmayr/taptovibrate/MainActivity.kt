5e11818f1e436efe7b3d5d248bdf8bcfaa3bf4b2
Dennis Mayr <dmayr.dev@gmail.com>
2022-08-01 22:48:05 -0400

    [refactor] ViewBinding implemented, refactored Vibrator access

--- app/src/main/java/dev/dmayr/taptovibrate/MainActivity.kt
+++ app/src/main/java/dev/dmayr/taptovibrate/MainActivity.kt
@@ -1,6 +1,5 @@
 package dev.dmayr.taptovibrate
 
-import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -9,7 +8,7 @@ import android.os.Vibrator
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.core.content.getSystemService
 import dev.dmayr.taptovibrate.databinding.ActivityMainBinding
 import java.time.Duration
 
@@ -21,9 +20,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        textView = findViewById(R.id.textView)
+        textView = binding.textView
         binding.apply {
             listOf(
                 textView,
@@ -41,8 +41,8 @@ class MainActivity : AppCompatActivity() {
 
     @Suppress("DEPRECATION")
     private fun vibratePhoneShort() {
-        val vibration = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        if (vibration.hasVibrator()) {
+        val vibration = getSystemService<Vibrator>()
+        if (vibration!!.hasVibrator()) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 vibration.vibrate(
                     VibrationEffect.createOneShot(
@@ -58,8 +58,8 @@ class MainActivity : AppCompatActivity() {
 
     @Suppress("DEPRECATION")
     private fun vibratePhoneLong() {
-        val vibration = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-        if (vibration.hasVibrator()) {
+        val vibration = getSystemService<Vibrator>()
+        if (vibration!!.hasVibrator()) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 vibration.vibrate(
                     VibrationEffect.createOneShot(
